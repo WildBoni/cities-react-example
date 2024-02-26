b86baa1b30253c5b7c17075dc4230691157a9334
Davide Bonifacio <web@davidebonifacio.it>
2024-02-26 14:35:09 +0100

    useState hook

--- src/City.jsx
+++ src/City.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import classes from './city.module.css'
 
 export default function City({name, continent, visited}) {
+  let [visitedCity, setVisitedCity] = useState(visited);
+  
+  function handleClick() {
+    setVisitedCity(!visitedCity) 
+    // true --> false 
+    // false ---> true
+  }
+  
   return (
     <div className={classes.city}>
-      <h2 className={visited ? classes.visited : classes.notvisited}>{name}</h2>
+      <h2 className={visitedCity ? classes.visited : classes.notvisited}>{name}</h2>
       <p>{continent}</p>
-      {visited && <h3>L'ho visitata!</h3>}
+      {visitedCity && <h3>L'ho visitata!</h3>}
+      <button onClick={handleClick}>Toggle visited</button>
     </div>
   )
 }
